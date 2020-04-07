d518f613f6689734b4fb7db1bc711a588c10752c
Dima <zyumalin@bk.ru>
2020-04-07 16:11:41 +0300

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
-var slider = tns({
-    container: '.feedback__carousel',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false
-  });
+$(document).ready(function() {
+    $('.feedback__carousel').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+    });
+}); 
