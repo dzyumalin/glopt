cedfeab47dad3682f3a3585124ec67c0139f19d6
Dima <zyumalin@bk.ru>
2020-04-22 14:37:45 +0300

    adaptive 768

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function() {
     $('.feedback__carousel').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
 
--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
     $('.feedback__carousel').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
+
+
 
 $(document).ready(function() {
     $('.hamburger__wrapper').click(function(event) {
