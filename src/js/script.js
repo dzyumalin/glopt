c17b2e019ce5f382719f6c67d00c545b075b7831
Dima <zyumalin@bk.ru>
2020-05-04 07:55:00 +0300

    adaptive modal, scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     });
 });
 
-// modal 
+// Modal window
 
 $('[data-modal=consultation]').on('click', function() {
     $('.overlay, #consultation').fadeIn('slow');
@@ -32,3 +32,43 @@ $('[data-modal=consultation]').on('click', function() {
 $('.modal__close').on('click', function() {
     $('.overlay, #consulatiton, #thanks').fadeOut('slow');
 });
+
+// Masked input
+
+$("input[name=phone]").mask("+7 (999) 999-99-99");
+
+$('form').submit(function(event) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+// Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+// fadein animation
+
+new WOW().init();
